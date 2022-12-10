755eb4f4c79d01097fcd3a4dfd2671960502bc6f
samueljohnprasadE <samgolla.johnprasad777@gmail.com>
2022-12-10 19:31:04 +0530

    Updates

---
+++ precache-manifest.47043e12a7d44a7d0872b9e165d79c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bd1576cd14ef92b712",
+    "url": "/cj-website/static/css/main.4572d92e.chunk.css"
+  },
+  {
+    "revision": "a1bd1576cd14ef92b712",
+    "url": "/cj-website/static/js/main.09cb41a3.chunk.js"
+  },
+  {
+    "revision": "9291224ae659d05688c4",
+    "url": "/cj-website/static/js/runtime~main.b0d60c59.js"
+  },
+  {
+    "revision": "900f26747a206813ae8c",
+    "url": "/cj-website/static/css/2.bfb9f5b2.chunk.css"
+  },
+  {
+    "revision": "900f26747a206813ae8c",
+    "url": "/cj-website/static/js/2.b598de5c.chunk.js"
+  },
+  {
+    "revision": "04fcbc96e1515d92805a1c05318d3ddd",
+    "url": "/cj-website/index.html"
+  }
+];
